2ffda169b0f4688e7e7da8959525330e132b5d71
wgx731 <wgx731@gmail.com>
2011-10-23 22:59:24 +0800

    modify port

--- angular-bottlepy.py
+++ angular-bottlepy.py
@@ -1,6 +1,10 @@
+import os
+import bottle
 import pywapi
 from bottle import route, run, static_file, redirect, request, error
 from beaker.middleware import SessionMiddleware
+
+bottle.debug(True)
 
 # session set up
 app = bottle.default_app()
@@ -70,4 +74,4 @@ def mistake404(code):
     return static_file('app/404.html', root='.')
 
 # start application
-run(app=app,host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+bottle.run(app=app,host='0.0.0.0', port=argv[1])
